5015867d1faaf2ed297827338c4acf3b284a921e
Sofía Islas <sofislas@stanford.edu>
2025-07-15 15:09:39 -0300

    feat: configuraciones para ejecucion con docker

---
+++ db/01-init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+export PGPASSWORD=$POSTGRES_PASSWORD;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /docker-entrypoint-initdb.d/testdb_dump.sql
+
